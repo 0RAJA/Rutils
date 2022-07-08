f5a86c254c40033520dfabd52d1c64c95a4f7c27
raja <1647193241@qq.com>
2022-07-08 10:12:25 +0800

    upload

--- pkg/setting/setting.go
+++ pkg/setting/setting.go
@@ -5,7 +5,7 @@ import (
 	"github.com/spf13/viper"
 )
 
-//使用viper进行配置文件的读取和热加载
+// 使用viper进行配置文件的读取和热加载
 
 type Setting struct {
 	vp *viper.Viper
@@ -16,27 +16,27 @@ type Setting struct {
 func NewSetting(configName, configType string, configPaths ...string) (*Setting, error) {
 	vp := viper.New()
 	vp.SetConfigName(configName)
-	vp.SetConfigType(configType) //设置配置文件类型
+	vp.SetConfigType(configType) // 设置配置文件类型
 	for _, config := range configPaths {
 		if config != "" {
-			vp.AddConfigPath(config) //可以设置多个配置路径,解决路径查找问题
+			vp.AddConfigPath(config) // 可以设置多个配置路径,解决路径查找问题
 		}
 	}
-	err := vp.ReadInConfig() //加载配置文件
+	err := vp.ReadInConfig() // 加载配置文件
 	if err != nil {
 		return nil, err
 	}
 	s := &Setting{vp: vp}
-	s.WatchSettingChange() //热监听
+	s.WatchSettingChange() // 热监听
 	return s, nil
 }
 
-//配置名存储记录
+// 配置名存储记录
 var sections = make(map[string]interface{})
 
 // ReadSection 绑定配置文件
 func (s *Setting) ReadSection(k string, v interface{}) error {
-	//绑定
+	// 绑定
 	err := s.vp.UnmarshalKey(k, v)
 	if err != nil {
 		return err
