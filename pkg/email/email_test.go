c4607232c150db803bfcbdc183baa200248a4e97
raja <1647193241@qq.com>
2022-07-08 10:17:43 +0800

    secert

--- pkg/email/email_test.go
+++ pkg/email/email_test.go
@@ -8,6 +8,16 @@ import (
 	"github.com/0RAJA/Rutils/pkg/times"
 )
 
+var (
+	Host     = "smtp.qq.com"
+	Port     = 465
+	UserName = "XXX@qq.com"
+	Password = ""
+	IsSSL    = true
+	From     = "XXX@qq.com"
+	To       = []string{"XXX@qq.com"}
+)
+
 /*
 Host: smtp.qq.com
   Port: 465
@@ -20,15 +30,15 @@ Host: smtp.qq.com
 */
 func TestEmail_SendMail(t *testing.T) {
 	defailtMailer := NewEmail(&SMTPInfo{
-		Host:     "smtp.qq.com",
-		Port:     465,
-		IsSSL:    true,
-		UserName: "1296643805@qq.com",
-		Password: "yfbxrbthozfnifjd",
-		From:     "1296643805@qq.com",
+		Host:     Host,
+		Port:     Port,
+		IsSSL:    IsSSL,
+		UserName: UserName,
+		Password: Password,
+		From:     From,
 	})
 	err := defailtMailer.SendMail( // 短信通知
-		[]string{"1296643805@qq.com"},
+		To,
 		fmt.Sprintf("异常抛出，发生时间: %s,%d", times.GetNowDateTimeStr(), time.Now().Unix()),
 		fmt.Sprintf("错误信息: %v", "NO"),
 	)
