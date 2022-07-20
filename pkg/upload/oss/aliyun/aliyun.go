6d0d61b076b37819244288229b2ee7440ec04380
raja <1647193241@qq.com>
2022-07-20 12:56:25 +0800

    upload增加删除多个文件

--- pkg/upload/oss/aliyun/aliyun.go
+++ pkg/upload/oss/aliyun/aliyun.go
@@ -56,20 +56,19 @@ func (o *OSS) UploadFile(file *multipart.FileHeader) (string, string, error) {
 
 // DeleteFile 删除文件
 // 通过key删除对应文件
-func (o *OSS) DeleteFile(key string) error {
+func (o *OSS) DeleteFile(key ...string) (oss.DeleteObjectsResult, error) {
 	bucket, err := o.newBucket()
 	if err != nil {
-		return errors.New("function OSS.NewBucket() Failed, err:" + err.Error())
+		return oss.DeleteObjectsResult{}, errors.New("function OSS.NewBucket() Failed, err:" + err.Error())
 	}
-
-	// 删除单个文件。objectName表示删除OSS文件时需要指定包含文件后缀在内的完整路径，例如abc/efg/123.jpg。
+	// 删除多个文件。objectName表示删除OSS文件时需要指定包含文件后缀在内的完整路径，例如abc/efg/123.jpg。
 	// 如需删除文件夹，请将objectName设置为对应的文件夹名称。如果文件夹非空，则需要将文件夹下的所有object删除后才能删除该文件夹。
-	err = bucket.DeleteObject(key)
+	result, err := bucket.DeleteObjects(key)
 	if err != nil {
-		return errors.New("function bucketManager.Delete() Filed, err:" + err.Error())
+		return oss.DeleteObjectsResult{}, errors.New("function bucketManager.Delete() Filed, err:" + err.Error())
 	}
 
-	return nil
+	return result, nil
 }
 
 func (o *OSS) newBucket() (*oss.Bucket, error) {
